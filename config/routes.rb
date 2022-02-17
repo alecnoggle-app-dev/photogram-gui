5f1b3dcf8a021e19595c4813c925bebe111b91a4
alecnoggle <anoggle@chicagobooth.edu>
2022-02-17 04:18:35 +0000

    Add works

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get("/photos", { :controller => "photo", :action => "landing_page" })
   get("/photos/:path_id", { :controller => "photo", :action => "photo_details" })
   get("/delete_photo/:path_id", { :controller => "photo", :action => "delete" })
+  get("/insert_photo", { :controller => "photo", :action => "add"})
 end
