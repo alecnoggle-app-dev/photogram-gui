755e01df1661b0cd1796ab7dccc8b43dc2314114
alecnoggle <anoggle@chicagobooth.edu>
2022-02-17 03:47:43 +0000

    Delete works

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -19,4 +19,14 @@ class PhotoController < ApplicationController
       render({ :template => "photo_templates/detail.html.erb" })
     end
   end
+
+  def delete
+    the_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/delete.html.erb"})
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photo", :action => "landing_page" })
   get("/photos/:path_id", { :controller => "photo", :action => "photo_details" })
+  get("/delete_photo/:path_id", { :controller => "photo", :action => "delete" })
 end
